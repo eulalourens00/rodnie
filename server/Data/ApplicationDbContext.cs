cde1b88e51d1700aa775d8f96daeccb4fcd3a120
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-09-10 14:34:42 +0300

    Добавил архитектурную основу, .env для работы с EF Core

---
+++ server/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rodnie.API.Models;
+
+namespace Rodnie.API.Data {
+    public class ApplicationDbContext : DbContext{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
