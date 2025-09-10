cb94bf2f677d4ad959c9cbeca4ab6d656c12baf6
rotat1ck <rotatick32151@gmail.com>
2025-09-10 16:09:12 +0300

    Основа для создания Rodnie.API (#4)
    * Update LICENSE (#3)
    
    * Добавил архитектурную основу, .env для работы с EF Core
    
    * Удалил временные файлы

---
+++ server/Controllers/Core/Auth/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rodnie.API.Controllers.Core.Auth {
+    [ApiController]
+    [Route("api/v1/core/auth/[controller]")]
+    public class RegisterController : Controller {
+        [HttpGet]
+        public IActionResult Index() {
+            return Ok("Registered");
+        }
+    }
+}
