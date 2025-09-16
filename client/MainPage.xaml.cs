66e8df1aa1dfe5f698d334cb4b41b0a666aa2013
rotat1ck <182702594+rotat1ck@users.noreply.github.com>
2025-09-16 14:47:36 +0300

    Дальнейшее развитие концепта

--- client/MainPage.xaml.cs
+++ client/MainPage.xaml.cs
@@ -7,16 +7,30 @@ public partial class MainPage : ContentPage {
     public MainPage() {
         InitializeComponent();
 
-        var moscowPos = new Location(55.7558, 37.6173);
-
-        map.MoveToRegion(MapSpan.FromCenterAndRadius(moscowPos, Distance.FromKilometers(10)));
-
-        var userPin = new Pin {
+        var collegePos = new Location(55.679154, 37.657988);
+        map.MoveToRegion(MapSpan.FromCenterAndRadius(collegePos, Distance.FromMeters(500)));
+        
+        var collegePin = new Pin {
             Label = "Пользователь",
-            Location = moscowPos,
+            Location = collegePos,
             Type = PinType.Place
         };
-        map.Pins.Add(userPin);
+        map.Pins.Add(collegePin);
+
+        Location shopPos = new Location(55.68264102278141, 37.66232979064646);
+        Pin shopPin = new Pin {
+            Type = PinType.SearchResult,
+            Label = "Магаз хз",
+            Location = shopPos
+        };
+        map.Pins.Add(shopPin);
+
+        map.MapElements.Add(new Circle {
+            Center = new Location(55.679154, 37.657988),
+            Radius = new Distance(50),
+            FillColor = Color.FromRgba(0, 255, 255, 0.5),
+            StrokeColor = Colors.Transparent
+        });
     }
 }
 
